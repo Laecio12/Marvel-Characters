bdc766837af7d741be700b934064b8e2c67c317e
Laecio Silva <laecio12silva@gmail.com>
2022-09-05 14:21:26 -0300

    feat: adicionando um evento de click no botão searchHero

--- app/src/main/java/com/laecio/marvelcharacters/presentation/view/SearchActivity.kt
+++ app/src/main/java/com/laecio/marvelcharacters/presentation/view/SearchActivity.kt
@@ -3,10 +3,12 @@ package com.laecio.marvelcharacters.presentation.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.laecio.marvelcharacters.R
+import com.laecio.marvelcharacters.data.HeroMarvelRepository
 import com.laecio.marvelcharacters.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
+    private val repository = HeroMarvelRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +17,17 @@ class SearchActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar).apply {
             title = getString(R.string.app_name)
+        }
+
+        binding.searchBtn.setOnClickListener {
+            val text = binding.searchBar.text.toString()
+
+            repository.searchByName(text,
+                {
+                 binding.title.text =   it.data.results[0].name
+                },
+                {}
+            )
         }
     }
 }
